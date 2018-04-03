c0687eb64eae99a583bed0c248f9a6af66a76d32
Aminata <ami.sy@hotmail.fr>
2018-04-03 22:48:40 +0200

    ajout dossier

--- main.js
+++ main.js
@@ -9,23 +9,23 @@ jQuery(function($){
   setDate();
   function setDate(){
     const now = new Date();
-    const s = ((launch.getTime() - now.getTime())/1000) - now.getTimezoneOffset()*60;
-    const d = Math.floor(s/86400);
+    var time = ((launch.getTime() - now.getTime())/1000) - now.getTimezoneOffset()*60;
+    const d = Math.floor(time/86400)
     /* 24*60*60 = 86400 */
     days.html('<strong>'+d+'</strong>Jour'+(d>1?'s':''));
-    s -= d*86400;
+    time -= d*86400;
 
-    const h = Math.floor(s/3600);
+    var h = Math.floor(time/3600);
     hours.html('<strong>'+h+'</strong>Heure'+(h>1?'s':''));
-    s -= h*3600;
+    time -= h*3600;
 
-    const m = Math.floor(s/60);
+     var m = Math.floor(time/60);
     minutes.html('<strong>'+m+'</strong>Minute'+(m>1?'s':''));
-    s -= m*60;
+     time -= m*60;
 
-    const s = Math.floor(s);
-    seconds.html('<strong>'+s+'</strong>Second'+(s>1?'s':''));
-    s -= s*60;
+    var s = Math.floor(time);
+    seconds.html('<strong>'+s+'</strong>Seconde'+(s>1?'s':''));
+    time -= s*60;
 
     setTimeout(setDate, 1000);
 
